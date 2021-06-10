539e74966439930bd6fad629a9d8014302240b8d
Ernest Arutiunian <arutiunian.ernest@gmail.com>
2021-06-10 11:37:20 -0400

    set up file write

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs")
  const inquirer = require("inquirer")
+ const generateMarkdown = ("./generateMarkdown.js").generateMarkdown
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -13,15 +14,73 @@ const questions = [
     "What is the licence you want to add to your project?",
     "What is your GitHub Account?",
     "What is your email address?",
+    
 ];
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile("./READ.ME", data, err => err ? console.log("Success!") : console.log("Failed to edit READ.ME"))
+    fs.writeFile("./READ.ME", generateMArkdown(data), err => err ? console.log("Success!") : console.log("Failed to edit READ.ME"))
 }
 
 // TODO: Create a function to initialize app
 function init() {
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: questions[0],
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: questions[1],
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: questions[2],
+      name: 'installation',
+    },
+    {
+        type: 'input',
+        message:  questions[3],
+        name: 'usage',
+    },
+    {
+    type: 'input',
+    message:  questions[4],
+    name: 'contribution',
+    },
+    {
+    type: 'input',
+    message:  questions[5],
+    name: 'instructions',
+    },
+    {
+    type: 'list',
+    name: 'license',
+    message: questions[6],
+    choices: ['Apache 2.0', 'MIT', 'GNU GPL v3', 'ISC'],
+    },
+
+    {
+    type: 'input',
+    message:  questions[7],
+    name: 'gitHub',
+    },
+    {
+    type: 'input',
+    message:  questions[8],
+    name: 'eamil',
+    },
+  ])
+  .then((response) =>
+        createHtml(response.uname, response.bio, response.linkedIn, response.gitHub)
+  );
+
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+let badge =''
 let link =''
 function renderLicenseBadge(license) {
   switch (license) {
     case  "Apache 2.0": 
-    link = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    badge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
       break;
     case "MIT": 
-    link = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
       break;
     case "GNU GPL v3": 
-    link = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+    badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
       break;
     case "ISC": 
-    link = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+    badge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
       break;
     default:
       break;
   }
+  return badge
 }
 
 
@@ -26,27 +28,29 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   switch (license) {
     case  "Apache 2.0" : 
-    link = "https://choosealicense.com/licenses/gpl-3.0/"
+    link = "[Apache 2.0] (https://choosealicense.com/licenses/gpl-3.0/)"
       break;
     case "MIT": 
-    link = "https://choosealicense.com/licenses/mit/"
+    link = "[MIT] (https://choosealicense.com/licenses/mit/)"
       break;
     case "GNU GPL v3": 
-    link = "https://choosealicense.com/licenses/isc/"
+    link = "[GNU GPL v3] (https://choosealicense.com/licenses/isc/)"
       break;
-    case "GNU GPL v3": 
-    link = "https://choosealicense.com/licenses/isc/"
+    case "ISC": 
+    link = "[ISC] (https://choosealicense.com/licenses/isc/)"
       break;
     default:
       break;
   }
+  return link
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-        renderLicenseBadge(license)
         renderLicenseLink(license)
+        renderLicenseBadge(license)
+        
 }
 
 // TODO: Create a function to generate markdown for README
@@ -59,12 +63,16 @@ function generateMarkdown(data) {
           - [Credits](#credits)
           - [License](#license) \n
           ## Installation \n
+          ${installation}\n
           ## Usage \n
+          ${usage}\n
           ## Credits \n
           ## License \n
           ${renderLicenseSection(data.license)}\n
           ## Features \n
+          
           ## How to Contribute \n
+          ${contribution}\n
           ## Tests \n
 `;
 }
